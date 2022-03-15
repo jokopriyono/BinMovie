dbb421df4b6b80bfbbdbb0fa1441d48ad125022b
marufr1 <marufrahman923@gmail.com>
2022-03-15 21:53:29 +0700

    Create database, model, and dao

---
+++ app/src/main/java/com/bin/movie/data/model/Example.kt
@@ -0,0 +1,14 @@
+package com.bin.movie.data.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "examples")
+@Parcelize
+data class Example(
+    @PrimaryKey(autoGenerate = true) var id: Long?,
+    @ColumnInfo var name: String
+) : Parcelable
