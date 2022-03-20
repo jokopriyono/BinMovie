2b205f22af59ee637110d6a77ed37e21f444a3ae
jokopriyono <joe.mandrake007@gmail.com>
2022-03-20 15:48:55 +0700

    add example map using osmdroid

--- app/src/main/java/com/bin/movie/ui/location/LocationFragment.kt
+++ app/src/main/java/com/bin/movie/ui/location/LocationFragment.kt
@@ -1,16 +1,43 @@
 package com.bin.movie.ui.location
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
 import androidx.fragment.app.Fragment
+import com.bin.movie.R
 import com.bin.movie.databinding.FragmentLocationBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.mylocation.IMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class LocationFragment : Fragment() {
 
     private var _binding: FragmentLocationBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Configuration.getInstance().load(
+            requireContext(),
+            requireContext().getSharedPreferences("osmdroid", Context.MODE_PRIVATE)
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +50,115 @@ class LocationFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.setTileSource(TileSourceFactory.MAPNIK)
+        binding.mapView.setMultiTouchControls(true)
+        binding.mapView.controller.zoomTo(15.0)
+        val coordinateMonas = GeoPoint(-6.1756259, 106.8265448)
+        binding.mapView.controller.animateTo(coordinateMonas, 15.0, 600L)
+
+        val locationOverlay = object : MyLocationNewOverlay(binding.mapView) {
+            override fun onLocationChanged(
+                location: Location?,
+                source: IMyLocationProvider?
+            ) {
+                location?.let {
+                    binding.txtLatitude.text = requireContext().resources.getString(
+                        R.string.latitude,
+                        it.latitude.toString()
+                    )
+                    binding.txtLongitude.text = requireContext().resources.getString(
+                        R.string.longitude,
+                        it.longitude.toString()
+                    )
+                }
+            }
+        }
+        locationOverlay.enableFollowLocation()
+        binding.mapView.overlayManager.add(locationOverlay)
+
+        requestLocationPermission()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 888
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 888
+                )
+            }
+        } else {
+            if (!isLocationEnabled1(requireContext())) {
+                AlertDialog.Builder(requireContext())
+                    .setCancelable(false)
+                    .setMessage("Location is required")
+                    .setPositiveButton(
+                        "Open Setting"
+                    ) { _, _ ->
+                        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                        startActivity(intent)
+                    }.show()
+            }
+        }
+    }
+
+    private fun isLocationEnabled1(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            888 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    if ((ContextCompat.checkSelfPermission(
+                            requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) ==
+                                PackageManager.PERMISSION_GRANTED)
+                    ) {
+                        Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
     }
 }
