2738f1e307d704034950c68df28b8ec99be68634
jokopriyono <joe.mandrake007@gmail.com>
2022-04-18 21:07:41 +0700

    add example modern intent result

--- app/src/main/java/com/bin/movie/ui/main/MainActivity.kt
+++ app/src/main/java/com/bin/movie/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bin.movie.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
@@ -8,6 +10,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.bin.movie.R
 import com.bin.movie.base.BaseActivity
 import com.bin.movie.databinding.ActivityMainBinding
+import com.bin.movie.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -17,11 +20,29 @@ class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
+    private val intentWithResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            // TODO ambil data dari intent -> it.data
+            // TODO atau ambil Activity.RESULT_OK atau Activity.RESULT_CANCELED
+            // it.data
+            // it.resultCode == Activity.RESULT_OK
+            // it.resultCode == Activity.RESULT_CANCELED
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /**
+         * Contoh intent modern
+         */
+        val intent = Intent(this, DetailActivity::class.java)
+        intentWithResult.launch(intent)
+        /**
+         * ====
+         */
 
         val navController = findNavController(R.id.nav_host_fragment_activity_example)
         binding.navView.setupWithNavController(navController)
